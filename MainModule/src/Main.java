50ba08a4c411d0f5c642d79805e9f5ecc08921c0
kiripiri <loredana.purcel999@gmail.com>
2020-02-25 17:08:30 +0200

    second commit lore

--- MainModule/src/Main.java
+++ MainModule/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("lore");
         System.out.println("Filess");
+        System.out.println("lore2");
+
     }
 }
